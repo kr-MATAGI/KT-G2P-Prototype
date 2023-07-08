23c60ba3556972245e97f66121813105dd027bd2
kr-MATAGI <dev_matagi@kakao.com>
2023-07-08 19:05:53 +0900

    상속 이용하여 각 테이블 생성하도록 변경

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,9 +1,88 @@
+import os
 import sqlite3
+
+from abc import *
 from db.sql_query import *
 from utils.post_method import PreDictItem
-from typing import Dict
+from typing import Dict, List
 
 DB_PATH = '../db/dict.db'
+
+#============================================
+class DB_Maker(metaclass=ABCMeta):
+#============================================
+    def __init__(
+            self,
+            db_path: str, table_name: str
+    ):
+        if not os.path.exists(db_path):
+            pass
+        print(f'{__class__} Set DB path: {db_path}')
+        self.db_path = db_path
+
+    def create_db(
+            self, db_path: str
+    ):
+        pass
+
+    @abstractmethod
+    def create_table(self):
+        pass
+
+    @abstractmethod
+    def insert_table_items(
+            self,
+            src_items: List
+    ):
+        pass
+
+    def select_all_items(self):
+        pass
+
+#============================================
+class HeadWord_DB_Maker(DB_Maker):
+#============================================
+    def __init__(
+            self,
+            db_path: str, table_name: str
+    ):
+        super(HeadWord_DB_Maker, self).__init__(db_path)
+
+#============================================
+class ConjuWord_DB_Maker(DB_Maker):
+#============================================
+    def __init__(
+            self,
+            db_path: str, table_name: str
+    ):
+        super(ConjuWord_DB_Maker, self).__init__(db_path=db_path, table_name=table_name)
+
+    def create_table(self):
+        pass
+
+    def insert_table_items(
+            self,
+            src_items: List
+    ):
+        pass
+
+#============================================
+class NNPWord_DB_Maker(DB_Maker):
+#============================================
+    def __init__(
+            self,
+            db_path: str, table_name: str
+    ):
+        super(NNPWord_DB_Maker, self).__init__(db_path=db_path, table_name=table_name)
+
+    def create_table(self):
+        pass
+
+    def insert_table_items(
+            self,
+            src_items: List
+    ):
+        pass
 
 #============================================
 def create_db(db_path: str):
@@ -46,4 +125,5 @@ def insert_items_to_db(db_path: str, dict_items: Dict[str, PreDictItem]):
 if '__main__' == __name__:
     print('[db_utils][__main__] MAIN')
 
-    create_db(db_path=DB_PATH)
+    db_maker = DB_Maker(db_path=DB_PATH)
+    headword_db_maker = HeadWord_DB_Maker(db_path=DB_PATH)
