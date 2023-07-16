280d420b204cb14260fcbe6669a13cf605121eb9
kr-MATAGI <dev_matagi@kakao.com>
2023-07-16 22:22:00 +0900

    update - 함수 구조 init

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -2,9 +2,14 @@ import os
 import sqlite3
 
 from abc import *
-from db.sql_query import *
+from db.sql_query import (
+    CREATE_TABLE_SQL, INSERT_ITEMS_SQL, SELECT_ALL_ITEMS
+)
 from utils.post_method import PreDictItem
 from typing import Dict, List
+from definition.db_def import (
+    HeadWordItems, ConjuWordItems, NnpWordItmes
+)
 
 DB_PATH = '../db/dict.db'
 
@@ -17,8 +22,8 @@ class DB_Maker(metaclass=ABCMeta):
     ):
         if not os.path.exists(db_path):
             raise Exception(f'[DB_Maker] ERR - DB_Path: {db_path}')
-        print(f'[DB_Maker]{table_name} Set DB path: {db_path}')
         self.db_path = db_path
+        self.table_name = table_name
 
     def create_db(
             self, db_path: str
@@ -51,6 +56,46 @@ class HeadWord_DB_Maker(DB_Maker):
             적용순위 : 3
         '''
         super(HeadWord_DB_Maker, self).__init__(db_path)
+        print(f'[db_utils][HeadWord_DB_Maker] db_path: {db_path}, table_name: {table_name}')
+
+    def create_table(self):
+        print(f'[db_utils][HeadWord_DB_Maker]: table_name: {self.table_name}')
+
+        conn = sqlite3.connect(self.table_name)
+        cursor = conn.cursor()
+
+        cursor.execute(CREATE_TABLE_SQL['CREATE_TABLE_SQL'])
+        conn.close()
+
+        print(f'[db_utils][HeadWord_DB_Maker] Complete !')
+
+    def insert_table_items(
+            self,
+            src_items: List[HeadWordItems]
+    ):
+        print(f'[db_utils][HeadWord_DB_Maker] table_name: {self.table_name}')
+
+        conn = sqlite3.connect(self.table_name)
+        cursor = conn.cursor()
+
+        insert_data_items = []
+        for idx, item in enumerate(src_items):
+            if 0 == (idx % 50000):
+                print(f'[db_utils][insert_table_items] table_name: {self.table_name}, {item.word}')
+
+            insert_data_items.append(('여기는 테이블 내의 column', '[여기에 db에 들어갈 데이터]'))
+        print(f'[db_utils][insert_table_items] all_items.size: {len(insert_data_items)}')
+        cursor.executemany(INSERT_ITEMS_SQL['HeadWord'], insert_data_items)
+        conn.commit()
+        cursor.execute(SELECT_ALL_ITEMS.format(self.table_name))
+
+        ''' db에 들어갔는지 확인 '''
+        temp_item_list = cursor.fetchall()
+        print(f'[db_utils][insert_table_items] CHECK - all_items.size: {len(temp_item_list)}')
+        print(temp_item_list[:5])
+
+        conn.close()
+        print(f'[db_utils][insert_table_items] Complete !')
 
 #============================================
 class ConjuWord_DB_Maker(DB_Maker):
@@ -64,6 +109,13 @@ class ConjuWord_DB_Maker(DB_Maker):
             적용순위 : 2
         '''
         super(ConjuWord_DB_Maker, self).__init__(db_path=db_path, table_name=table_name)
+
+        print(f'[db_utils][ConjuWord_DB_Maker] db_path: {db_path}, table_name: {table_name}')
+        if not os.path.exists(db_path):
+            raise Exception('ERR - DB_PATH')
+
+        self.db_path = db_path
+        self.table_name = table_name
 
     def create_table(self):
         pass
@@ -86,6 +138,7 @@ class NNPWord_DB_Maker(DB_Maker):
             적용순위 : 1
         '''
         super(NNPWord_DB_Maker, self).__init__(db_path=db_path, table_name=table_name)
+        self.db_path = db_path
 
     def create_table(self):
         pass
@@ -96,46 +149,14 @@ class NNPWord_DB_Maker(DB_Maker):
     ):
         pass
 
-#============================================
-def create_db(db_path: str):
-#============================================
-    print(f'[db_utils][create_db] db_path: {db_path}')
-
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    cursor.execute(CREATE_SQL)
-
-    conn.close()
-
-#============================================
-def insert_items_to_db(db_path: str, dict_items: Dict[str, PreDictItem]):
-#============================================
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    insert_data_list = []
-    for idx, (key, dict_item) in enumerate(dict_items.items()):
-        if 0 == (idx % 50000):
-            print(f'[db_utils][insert_items_to_db] {idx} is processing... {key}')
-
-        concat_pronun_list = ','.join(dict_item.pronun_list)
-        insert_data_list.append((key, concat_pronun_list, dict_item.pos))
-    print(f'[db_utils][insert_items_to_db] all_items.size: {len(insert_data_list)}')
-    cursor.executemany(INSERT_SQL, insert_data_list)
-    conn.commit()
-
-    cursor.execute(SELECT_ALL_ITEMS)
-    item_list = cursor.fetchall()
-    print(f'[db_utils][insert_items_to_db] check all_items.size: {len(item_list)}')
-    print(item_list[:5])
-
-    conn.close()
-
-
 ### MAIN ###
 if '__main__' == __name__:
     print('[db_utils][__main__] MAIN')
+
+    '''
+        어떤 기분석 사전 DB를 만들지 설정
+    '''
+
 
     db_maker = DB_Maker(db_path=DB_PATH)
     headword_db_maker = HeadWord_DB_Maker(db_path=DB_PATH)
