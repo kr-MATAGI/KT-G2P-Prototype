87d47a13c208bdca83a5061e09e5d45019007ac1
kr-MATAGI <dev_matagi@kakao.com>
2023-07-16 22:22:19 +0900

    기분석 사전 구조 init

---
+++ definition/db_def.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class HeadWordItems:
+    word: str = ''
+
+@dataclass
+class ConjuWordItems:
+    word: str = ''
+
+@dataclass
+class NnpWordItmes:
+    word: str = ''
